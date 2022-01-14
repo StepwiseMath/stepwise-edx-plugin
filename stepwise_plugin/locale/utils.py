d87766163333e713791450a3e8c09dc99fc94f77
Lawrence McDaniel <lpm0073@gmail.com>
2022-01-14 15:24:04 -0600

    code anchor localizer

--- stepwise_plugin/locale/utils.py
+++ stepwise_plugin/locale/utils.py
@@ -6,9 +6,13 @@ StepWise theming utility functions
 """
 
 from ast import Str
+from openedx.core.djangoapps.lang_pref import LANGUAGE_KEY
+from openedx.core.djangoapps.user_api.preferences.api import get_user_preference
 
+# this repo
+from stepwise_plugin.models import Locale
 
-def anchor(user, id: Str):
+def anchor(user, element_id: Str):
     """
     id: an html anchor tag id value
     example: stepwise-about
@@ -16,5 +20,24 @@ def anchor(user, id: Str):
     returns the URL and anchor element value based on the user's
     example
     """
-    if not user.is_authenticated:
-        prefered_language = "en"
+    prefered_language = "en"
+    if user.is_authenticated:
+        prefered_language = get_user_preference(user, LANGUAGE_KEY)
+
+    locale = Locale.objects.filter(element_id=element_id, lang=prefered_language).first()
+    if not locale:
+        # try a simpler variation of the language code.
+        prefered_language = prefered_language[:2]
+        locale = Locale.objects.filter(element_id=element_id, lang=prefered_language).first()
+
+    if not locale:
+        # try to grab the element in English.
+        locale = Locale.objects.filter(element_id=element_id, lang="en").first()
+
+    if not locale:
+        return {}
+    
+    return {
+        "url": locale.url,
+        "value": locale.value
+    }
