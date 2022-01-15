323fcafb111e59e0c4c5ea8015c013ae6773fbc7
Lawrence McDaniel <lpm0073@gmail.com>
2022-01-15 11:41:12 -0600

    standardize the list presentation of each model

--- stepwise_plugin/admin.py
+++ stepwise_plugin/admin.py
@@ -5,24 +5,23 @@ from .models import Configuration, Locale, MarketingSites
 
 class MarketingSitesAdmin(admin.ModelAdmin):
     list_display = [f.name for f in MarketingSites._meta.get_fields()]
+    readonly_fields = (
+        u"created",
+        u"updated",
+    )
 
 
 class LocaleAdmin(admin.ModelAdmin):
-    list_display = (
-        "element_id",
-        "language",
-        "url",
-        "value",
+    list_display = [f.name for f in Locale._meta.get_fields()]
+    readonly_fields = (
+        u"created",
+        u"updated",
     )
 
 
 class ConfigurationAdmin(admin.ModelAdmin):
-    list_display = (
-        "type",
-        "stepwise_host",
-        "created",
-        "updated",
-    )
+    list_display = [f.name for f in Configuration._meta.get_fields()]
+
     readonly_fields = (
         u"created",
         u"updated",
