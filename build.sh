3e48bc14062958d43c11026f049e2b28bd01dba4
lpm0073 <lpm0073@gmail.com>
2022-10-12 12:33:20 -0500

    refactor build/release scripts

--- build.sh
+++ build.sh
@@ -11,10 +11,21 @@
 #
 #------------------------------------------------------------------------------
 
-./build.sh
+python3 -m pip install --upgrade setuptools wheel twine
+python -m pip install --upgrade build
+
+sudo rm -r build
+sudo rm -r dist
+sudo rm -r stepwise_edx_plugin.egg-info
+
+python3 -m build --sdist ./
+python3 -m build --wheel ./
+
+python3 -m pip install --upgrade twine
+twine check dist/*
 
 # PyPi test
-twine upload --skip-existing --repository testpypi dist/*
+#twine upload --skip-existing --repository testpypi dist/*
 
 # PyPi
 #twine upload --skip-existing dist/*
