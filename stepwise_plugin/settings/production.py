5be51210bcb13ea2362de782052193b8f3304efb
lpm0073 <lpm0073@gmail.com>
2022-10-18 10:56:33 -0500

    add django-memberpress-client

--- stepwise_plugin/settings/production.py
+++ stepwise_plugin/settings/production.py
@@ -10,7 +10,15 @@ def plugin_settings(settings):
 
     # MemberPress REST API client
     # -------------------------------------------------------------------------
-    MEMBERPRESS_API_KEY = "set-me-please"                           # noqa: F841
-    MEMBERPRESS_API_BASE_URL = "https://set-me-please.com/"         # noqa: F841
-    MEMBERPRESS_CACHE_EXPIRATION = 300                              # noqa: F841
-    MEMBERPRESS_SENSITIVE_KEYS = ["password", "token", "client_id", "client_secret", "Authorization", "secret"]  # noqa: F841
+    settings.MEMBERPRESS_API_KEY = "set-me-please"  # noqa: F841
+    settings.MEMBERPRESS_API_BASE_URL = "https://stepwisemath.ai"  # noqa: F841
+    settings.MEMBERPRESS_CACHE_EXPIRATION = 60 * 60 * 24  # noqa: F841
+    settings.MEMBERPRESS_API_KEY_NAME = "MEMBERPRESS-API-KEY"  # noqa: F841
+    settings.MEMBERPRESS_SENSITIVE_KEYS = [
+        "password",
+        "token",
+        "client_id",
+        "client_secret",
+        "Authorization",
+        "secret",
+    ]  # noqa: F841
