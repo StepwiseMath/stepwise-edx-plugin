fa34b0beb44f19efe071abf04ecd99da7509806e
lpm0073 <lpm0073@gmail.com>
2022-11-08 18:55:11 -0600

    reconfigure oauth backend for use with miniOrange OAuth / OpenID Connect Server

--- stepwise_plugin/stepwise_oauth.py
+++ stepwise_plugin/stepwise_oauth.py
@@ -12,6 +12,12 @@ class StepwiseMathWPOAuth2(WPOpenEdxOAuth2):
     # the "Client ID:" and "Client Secret:" fields.
     name = "stepwisemath-oauth"
 
-    # note: no slash at the end of the base url. Python Social Auth
-    # might clean this up for you, but i'm not 100% certain of that.
-    BASE_URL = "https://stepwisemath.ai/wp-json/moserver"
+    # setup oauth endpoints:
+    # authorization:    https://stepwisemath.ai/wp-json/moserver/authorize
+    # token:            https://stepwisemath.ai/wp-json/moserver/token
+    # user info:        https://stepwisemath.ai/wp-json/moserver/resource
+    BASE_URL = "https://stepwisemath.ai"
+    PATH = "wp-json/moserver"
+    AUTHORIZATION_ENDPOINT = "authorize"
+    TOKEN_ENDPOINT = "token"
+    USERINFO_ENDPOINT = "resource"
