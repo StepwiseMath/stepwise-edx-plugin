0c8fb7c93a54aa973485beafe9d2db5c1fce84dd
lpm0073 <lpm0073@gmail.com>
2024-09-27 19:25:04 -0600

    fix: urls is deprecated in favor of re_path

--- stepwise_plugin/urls.py
+++ stepwise_plugin/urls.py
@@ -7,7 +7,8 @@ https://web.stepwisemath.ai/stepwise/dashboard
 https://web.stepwisemath.ai/stepwise/dashboard?language=es-419
 """
 # Django
-from django.conf.urls import url
+from django.urls import re_path
+
 
 # this repo
 from stepwise_plugin.dashboard.views import student_dashboard
@@ -17,6 +18,6 @@ from stepwise_plugin.api.urls import urlpatterns as api_urlpatterns
 app_name = "stepwise_plugin"
 
 urlpatterns = [
-    url(r"^dashboard/?$", student_dashboard, name="stepwise_dashboard"),
-    url(r"^marketing-redirector/?$", marketing_redirector, name="stepwise_marketing_redirector"),
+    re_path(r"^dashboard/?$", student_dashboard, name="stepwise_dashboard"),
+    re_path(r"^marketing-redirector/?$", marketing_redirector, name="stepwise_marketing_redirector"),
 ] + api_urlpatterns
